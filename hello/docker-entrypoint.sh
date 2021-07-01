234236433eff3b003b7ab8998085b9c77a20c637
0x4448 <86135470+0x4448@users.noreply.github.com>
2021-06-30 20:41:13 -0700

    Remove app name from env vars

--- hello/docker-entrypoint.sh
+++ hello/docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if echo $HELLO_STARTUP_DELAY | grep -qE '^[0-9]+$'; then
-    sleep $HELLO_STARTUP_DELAY
+if echo $STARTUP_DELAY | grep -qE '^[0-9]+$'; then
+    sleep $STARTUP_DELAY
 fi
 
 gunicorn --bind 0.0.0.0:8000 hello:app
