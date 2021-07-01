33a9aa602e76c0d4bd6b2fa34c705f5b07d7501b
0x4448 <86135470+0x4448@users.noreply.github.com>
2021-06-30 21:19:24 -0700

    Add random error generation env var

--- hello/hello.py
+++ hello/hello.py
@@ -1,13 +1,30 @@
 #!/usr/bin/env python3
+import os
 import platform
+import random
 
-from flask import Flask, jsonify, request
+from flask import Flask, abort, jsonify, request
 
 app = Flask(__name__)
 
 
+# Error handlers
+@app.errorhandler(500)
+def internal_server_error(e):
+    return jsonify(error=str(e)), 500
+
+
+# Views
 @app.route("/")
 def index():
+    # Return 500 occasionally if environment variable ERROR_RATE is defined.
+    try:
+        if float(os.environ.get("ERROR_RATE", 0)) > random.random() * 100:
+            abort(500, description="randomly generated error")
+    except ValueError:
+        # Ignore invalid values for ERROR_RATE.
+        pass
+
     response = {
         "client_ip": request.remote_addr,
         "hostname": platform.node(),
